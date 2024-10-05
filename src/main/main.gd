5cea31a382dc4e395e081b781a7889badcd8c924
Rowan Althorp <rowalthorp@gmail.com>
2024-10-05 15:42:27 -0700

    added smite, and added some more logic to that

--- src/main/main.gd
+++ src/main/main.gd
@@ -4,7 +4,6 @@ var mainMenu
 var paused = false
 var pauseMenuScene = load("res://src/main/Pause Menu.tscn")
 var pauseMenu
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	mainMenu = get_node("MainMenu")
@@ -13,7 +12,7 @@ func startGame() -> void:
 	mainMenu.queue_free()
 
 func _input(_ev):
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_key_pressed(KEY_ESCAPE):
 		togglePause()
 	
 func togglePause() -> void:
