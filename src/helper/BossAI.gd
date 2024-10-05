d740daf492544e6991acb691d6f22a68d20321d9
BericMoeller <bericmoeller@gmail.com>
2024-10-04 17:36:59 -0700

    Created a class for bosses
    -just basic framework
    -make sure we use these functions so all code runs smoothly

---
+++ src/helper/BossAI.gd
@@ -0,0 +1,27 @@
+class_name Boss extends Node2D
+
+var velocity;
+
+#put reference to player here \/
+#var player; 
+
+
+# Called when the node enters the scene tree for the first time.
+
+func main_attack() -> void: 
+	pass
+	
+func aoe_attack() -> void:
+	pass
+	
+func projectile_attack() -> void:
+	pass
+	
+func move() -> void:
+	pass
+	
+func take_damage() -> void:
+	pass
+	
+func make_decisions() -> void:
+	pass
