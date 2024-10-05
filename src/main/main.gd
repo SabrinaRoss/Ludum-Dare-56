1bdee06cb2d20530c24927ab9d9f26f7b7f1eec2
MstrMinecrafter <bingbong621@gmail.com>
2024-10-05 15:40:25 -0700

    Added escape key to input map
    Fixes flickering pause menu bug

--- src/main/main.gd
+++ src/main/main.gd
@@ -4,6 +4,7 @@ var mainMenu
 var paused = false
 var pauseMenuScene = load("res://src/main/Pause Menu.tscn")
 var pauseMenu
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	mainMenu = get_node("MainMenu")
@@ -12,7 +13,7 @@ func startGame() -> void:
 	mainMenu.queue_free()
 
 func _input(_ev):
-	if Input.is_key_pressed(KEY_ESCAPE):
+	if Input.is_action_just_pressed("pause"):
 		togglePause()
 	
 func togglePause() -> void:
