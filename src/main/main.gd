ec32395a56865b7e1d824aa98d306a87d85177f1
MstrMinecrafter <bingbong621@gmail.com>
2024-10-05 22:56:56 -0700

    Included death condition

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-var mainMenu
 var paused = false
 var pauseMenuScene = load("res://src/main/Pause Menu.tscn")
 var pauseMenu
+var deathAnimationPlaying = false
+var deathAnimationShrinkSpeed = 0.01
 
-var level
+var level = 0
+var mainMenuScene = load("res://src/main/Main Menu.tscn")
 var level1Scene = load("res://src/main/Level 1.tscn")
 var level2Scene = load("res://src/main/Level 2.tscn")
 var level3Scene = load("res://src/main/Level 3.tscn")
@@ -13,12 +15,9 @@ var gameScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	mainMenu = get_node("MainMenu")
 	process_mode = PROCESS_MODE_ALWAYS
-	
-func startGame() -> void:
-	mainMenu.queue_free()
-	setLevel(1)
+	Singleton.main = self
+	setLevel(0)
 
 func _input(_ev):
 	if Input.is_action_just_pressed("pause"):
@@ -28,7 +27,7 @@ func togglePause() -> void:
 	if paused:
 		pauseMenu.queue_free()
 		paused = false
-	elif mainMenu == null:
+	elif level == 0:
 		pauseMenu = pauseMenuScene.instantiate()
 		pauseMenu.main = self
 		add_child(pauseMenu)
@@ -39,7 +38,9 @@ func setLevel(newLevel) -> void:
 	if gameScene != null:
 		gameScene.queue_free()
 	level = newLevel
-	if level == 1:
+	if level == 0:
+		gameScene = mainMenuScene.instantiate()
+	elif level == 1:
 		gameScene = level1Scene.instantiate()
 	elif level == 2:
 		gameScene = level2Scene.instantiate()
@@ -47,3 +48,15 @@ func setLevel(newLevel) -> void:
 		gameScene = level3Scene.instantiate()
 	add_child(gameScene)
 	gameScene.process_mode = PROCESS_MODE_PAUSABLE
+	
+func death() -> void:
+	get_tree().paused = true
+	deathAnimationPlaying = true
+
+func _process(_delta: float) -> void:
+	if deathAnimationPlaying:
+		Singleton.player.scale -= Vector2(1,1) * deathAnimationShrinkSpeed
+		if Singleton.player.scale.x <= 0:
+			deathAnimationPlaying = false
+			get_tree().paused = false
+			setLevel(0)
