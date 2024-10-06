7b515e068e94ffaaf794ab91dea76ae72abacc02
Avery Simmons <eno116116@gmail.com>
2024-10-06 00:40:23 -0700

    squirrel

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
 var paused = false
-var pauseMenuScene = load("res://src/main/Pause Menu.tscn")
+var pauseMenuScene = preload("res://src/main/Pause Menu.tscn")
 var pauseMenu
 var deathAnimationPlaying = false
 var deathAnimationShrinkSpeed = 0.01
 
 var level = 0
-var mainMenuScene = load("res://src/main/Main Menu.tscn")
-var level1Scene = load("res://src/main/Level 1.tscn")
-var level2Scene = load("res://src/main/Level 2.tscn")
-var level3Scene = load("res://src/main/Level 3.tscn")
+var mainMenuScene = preload("res://src/main/Main Menu.tscn")
+var level1Scene = preload("res://src/main/Level 1.tscn")
+var level2Scene = preload("res://src/main/Level 2.tscn")
+var level3Scene = preload("res://src/main/Level 3.tscn")
 var gameScene
 
 var curEffectsNode
