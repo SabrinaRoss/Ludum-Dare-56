ddd78ef08e5cc11f3080ecfa86148f99e40e9559
Rowan Althorp <rowalthorp@gmail.com>
2024-10-06 22:54:44 -0700

    beaver donegit add .

--- src/entities/boss/Beetle/BeetleAI.gd
+++ src/entities/boss/Beetle/BeetleAI.gd
@@ -31,7 +31,7 @@ Direction:
 func _ready() -> void:
 	vel = Vector2.ZERO
 	screenSize = get_viewport_rect().size
-	health = 1.0
+	health = 100.0
 	speed = 45.0
 	aoeCollider = $Rotate/aoe
 	mainAttackCollider = $Rotate/main_attack
@@ -279,7 +279,7 @@ func take_damage(damage: float) -> void:
 		death()
 
 func death():
-	Singleton.main.bossDeath()
+	pass
 
 func make_decisions() -> void:
 	match state:
